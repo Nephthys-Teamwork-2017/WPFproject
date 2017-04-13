30ebd575d0f6e629ce2a481a2efb1bc9553b228c
yordank <yordankp@gmail.com>
2017-04-13 21:46:13 +0300

    simple menu update

---
+++ WpfApplication2/WpfApplication2/Migrations/201704131238315_InitialCreate.cs
@@ -0,0 +1,31 @@
+namespace WpfApplication2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clients",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Address = c.String(),
+                        Phone = c.String(),
+                        Email = c.String(),
+                        Notes = c.String(),
+                        StatePersonalNumber = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Clients");
+        }
+    }
+}
